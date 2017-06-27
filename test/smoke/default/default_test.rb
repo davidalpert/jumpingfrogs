fb4959dc53856e58299a11ecb01691c74b4b4a50
David Alpert <david.alpert@northfieldit.com>
2017-06-27 11:00:19 -0500

    first steps: a simple recipe with tests

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -7,12 +7,20 @@
 
 unless os.windows?
   # This is an example test, replace with your own test.
-  describe user('root'), :skip do
+  describe user('root') do
     it { should exist }
   end
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
+describe port(80) do
   it { should_not be_listening }
+end
+
+filepath = 'c:\users\vagrant\desktop\foo.txt' if os.windows?
+filepath = '/home/vagrant/foo.txt' unless os.windows?
+
+describe file(filepath) do
+  it { should exist }
+  its('content') { should match ('bar') }
 end
